a0c867451bb3dc3f1a2da5852312750ef2d1756a
Hamza Yousaf <hamzayousaf056@gmail.com>
2023-02-10 15:25:11 +0500

    changes

--- app/hello/index.tsx
+++ app/hello/index.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 const Index = () =>{
     return(
         <div>
-            Hello There!
+            Hello There! 123
             <Link href={"/second"}>Go to Second Page</Link>
         </div>
     )
